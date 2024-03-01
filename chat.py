4d3112bb77fb8626769c1cc30be6067287930deb
Nero Li <1109680304lym@gmail.com>
2024-03-01 13:33:51 -0800

    Working on chat.py

--- chat.py
+++ chat.py
@@ -2,11 +2,9 @@ import os
 import openai
 from datetime import datetime
 from dotenv import load_dotenv, find_dotenv
+from update import createVector
 
-from langchain.document_loaders.csv_loader import CSVLoader
 from langchain_openai import ChatOpenAI
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.memory import ConversationBufferMemory
@@ -20,6 +18,7 @@ class ChatBot:
         self.response_content = response_content
         self.user_contents = []
         self.assistant_responses = []
+        self_userdb = createVector()
 
         _ = load_dotenv(find_dotenv()) # read local .env file
         self.openai.api_key  = os.environ['OPENAI_API_KEY']
@@ -30,49 +29,10 @@ class ChatBot:
         file = open(name, 'r', encoding = 'utf-8', errors='ignore')
         return file.read()
 
-def get_respond(messages) -> str:
-  completion = openai.ChatCompletion.create(
-    model="gpt-4",
-    messages=messages, 
-  )
+    def get_respond(messages) -> str:
+        completion = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=messages, 
+        )
 
-  return completion['choices'][0]['message']['content']
-
-def call_gpt():
-  currentTime = datetime.now()
-  outputFileName = currentTime.strftime("%y%m%d%H%M%S-") + args.response
-  out = open(outputFileName, 'w', encoding = 'utf-8', errors='ignore')
-  user_contents = []
-  assistant_responses = []
-
-  system_content = get_file_content(args.system)
-  if args.mode == 'file':
-    user_content = get_file_content(args.prompt)
-    print(f'Human: {user_content}')
-  else:
-    user_content = input('Human: ')  
-  user_contents.append(user_content)
-
-  while True:
-    out.write("Human:\n" + user_content + "\n\n")
-    messages = [{"role": "system", "content": system_content}]
-    messages = []
-    for i in range(len(assistant_responses)):
-      messages.append({"role": "user", "content": user_contents[i]})
-      messages.append({"role": "assistant", "content": assistant_responses[i]})
-
-    messages.append({"role": "user", "content": user_contents[len(user_contents) - 1]})
-
-    response = get_respond(messages)
-    print('AI:', response)
-    out.write("AI:\n" + response + "\n\n")
-    assistant_responses.append(response)
-
-    user_content = input('Human: ')
-    if user_content == 'exit':
-      break
-    user_contents.append(user_content)
-      
-if __name__ == '__main__':
-  args = arguments()
-  call_gpt()
+        return completion['choices'][0]['message']['content']
