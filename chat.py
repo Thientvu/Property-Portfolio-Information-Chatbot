5e5877db866852aaa8868627ac605404e55b4f7b
Nero Li <1109680304lym@gmail.com>
2024-03-01 19:50:09 -0800

    Finished chat.py

--- chat.py
+++ chat.py
@@ -12,27 +12,40 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
 
 class ChatBot:
-    def __init__(self, system_content, user_content, response_content):
-        self.system_content = system_content
-        self.user_content = user_content
-        self.response_content = response_content
-        self.user_contents = []
-        self.assistant_responses = []
-        self_userdb = createVector()
+    def __init__(self, user_file):
+        self.user_file = user_file
+        self.userdb = createVector(user_file)
 
         _ = load_dotenv(find_dotenv()) # read local .env file
         self.openai.api_key  = os.environ['OPENAI_API_KEY']
         self.llm_name = 'gpt-4-turbo-preview'
         self.llm = ChatOpenAI(model_name = self.llm_name, temperature = 0)
-
-    def get_file_content(name: str) -> str:
-        file = open(name, 'r', encoding = 'utf-8', errors='ignore')
-        return file.read()
-
-    def get_respond(messages) -> str:
-        completion = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=messages, 
+        self.memory = ConversationBufferMemory(
+            memory_key="chat_history",
+            return_messages=True
         )
 
-        return completion['choices'][0]['message']['content']
+    def getRespond(self, messages) -> str:
+        template = """Use the following pieces of context to answer the question at the end. 
+        If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+        Provide a thorough explanation of your reasoning, using bullet points for clarity where needed.
+        Conclude each response with 'Thank you for asking, Is there anything else I can help you with?' placed 1 line after the answer.
+
+        {context}
+
+        Return the output in the following format: 
+        Question: {question}
+        Answer:
+
+        Thank you for asking, Is there anything else I can help you with?
+        """
+        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template,)
+        qa = ConversationalRetrievalChain.from_llm(
+            self.llm,
+            retriever=self.userdb.as_retriever(search_type="similarity", search_kwargs={"k": 3}),
+            memory=self.memory,
+            verbose = True,
+            combine_docs_chain_kwargs={'prompt': QA_CHAIN_PROMPT}
+)
+        result = qa({"question": messages})
+        return result['answer']
