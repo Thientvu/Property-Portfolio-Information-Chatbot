8ddda80f8d0d3fc06b97863c99ac435cfdaa9763
Thien Vu <briantvu12316@gmail.com>
2024-03-03 18:50:27 -0800

    Combined all the code together, chatbot works"

--- chat.py
+++ chat.py
@@ -1,48 +1,52 @@
 import os
 import openai
-from datetime import datetime
 from dotenv import load_dotenv, find_dotenv
-from update import createVector
-
+from vectordb import createVector
 from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
 
 class ChatBot:
-    def __init__(self, user_file):
-        self.user_file = user_file
+    def __init__(self, user_file, memory):
+        self. user_file = user_file
+
         self.userdb = createVector(user_file)
 
         _ = load_dotenv(find_dotenv()) # read local .env file
-        self.openai.api_key  = os.environ['OPENAI_API_KEY']
+        self.openai_api_key  = os.environ['OPENAI_API_KEY']
+
         self.llm_name = 'gpt-4-turbo-preview'
-        self.llm = ChatOpenAI(model_name = self.llm_name, temperature = 0)
-        self.memory = ConversationBufferMemory(
-            memory_key="chat_history",
-            return_messages=True
-        )
 
-    def getRespond(self, messages) -> str:
-        template = """Use the following pieces of context to answer the question at the end. 
-        If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-        Provide a thorough explanation of your reasoning, using bullet points for clarity where needed.
+        self.llm = ChatOpenAI(model_name = self.llm_name, openai_api_key = self.openai_api_key, temperature = 0)
 
+        self.template = """
+        Based on the chat history and context provided, Do your best to answer the question.
+        If the answer is uncertain, return 'I'm not sure, please contact our representative'. It's important not to make up an answer.
+        Reason before answer. Keep the answer straighforward and concise.
+        If the question involves calculations, outline the step-by-step process to derive the results.
+        After each response, place 'Thank you for asking, Is there anything else I can help you with?' 1 line after the answer.
+
+        Question:
+        {question}
+
+        Chat History:
+        {chat_history}
+
+        Context:
         {context}
-
-        Return the output in the following format: 
-        Question: {question}
-        Answer:
         """
-        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],template=template,)
-        qa = ConversationalRetrievalChain.from_llm(
+
+        self.QA_CHAIN_PROMPT = PromptTemplate(input_variables=['question'], template=self.template)
+
+        self.qa = ConversationalRetrievalChain.from_llm(
             self.llm,
             retriever=self.userdb.as_retriever(search_type="similarity", search_kwargs={"k": 3}),
-            memory=self.memory,
+            memory=memory,
             verbose = True,
-            combine_docs_chain_kwargs={'prompt': QA_CHAIN_PROMPT}
-)
-        result = qa({"question": messages})
+            combine_docs_chain_kwargs={'prompt': self.QA_CHAIN_PROMPT}
+        )
+
+
+    def getRespond(self, messages) -> str:
+        result = self.qa({"question": messages})
         return result['answer']
--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-import init
-import update
-import generate
-import chat
-import main
-import sys
+from chat import ChatBot    
+from langchain.chains.conversation.memory import ConversationBufferMemory
+
+FILE = 'dat/526963_processed_data.csv'
+
+def main():
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages = True)
+    chatbot = ChatBot(user_file = FILE, memory = memory)
+
+    question1 = 'Who is the client in the report?'
+    question2 = 'What is their name?'
+    question3 = 'What is their first name?'
+
+    print(chatbot.getRespond(question1))
+    print(chatbot.getRespond(question2))
+    print(chatbot.getRespond(question3))
 
 if __name__ == '__main__':
-    print(init.load_all_data())
+     main()
